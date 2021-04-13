74dfa4983b764fd71847fc6e0ed032ec67f46af9
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-04-13 19:05:38 +0900

    メモの保存機能

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -2,65 +2,60 @@ import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import {
-  View, Text, StyleSheet, TouchableOpacity, Alert,
+  View, Text, StyleSheet, TouchableOpacity, Alert, FlatList,
 } from 'react-native';
+import {
+  shape, string, instanceOf, arrayOf,
+} from 'prop-types';
 
-export default function MemoList() {
+export default function MemoList(props) {
+  // memosの値を取り出す
+  const { memos } = props;
   // 子供のコンポーネントの場合はnavigationは使えないのでuseNacigationを使う
   const navigation = useNavigation();
+
+  function renderItem({ item }) {
+    return (
+      <TouchableOpacity
+        style={styles.memoListItem}
+        onPress={() => { navigation.navigate('MemoDitail'); }}
+      >
+        <View>
+          {/* numberOfLinesはタイトルを１行だけにするための処理 */}
+          <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
+          <Text style={styles.memoListItemDate}>{String(item.updateAt)}</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.memoDelete}
+          onPress={() => { Alert.alert('Are you sure?'); }}
+        >
+          <Feather name="x" size={16} color="#fff" />
+        </TouchableOpacity>
+      </TouchableOpacity>
+    );
+  }
+
   return (
     <View>
-      <TouchableOpacity
-        style={styles.memoListItem}
-        onPress={() => { navigation.navigate('MemoDitail'); }}
-      >
-        <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-        </View>
-        <TouchableOpacity
-          style={styles.memoDelete}
-          onPress={() => { Alert.alert('Are you sure?'); }}
-        >
-          <Feather name="x" size={16} color="#fff" />
-        </TouchableOpacity>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.memoListItem}
-        onPress={() => { navigation.navigate('MemoDitail'); }}
-      >
-        <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-        </View>
-        <TouchableOpacity
-          style={styles.memoDelete}
-          onPress={() => { Alert.alert('Are you sure?'); }}
-        >
-          <Feather name="x" size={16} color="#fff" />
-        </TouchableOpacity>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.memoListItem}
-        onPress={() => { navigation.navigate('MemoDitail'); }}
-      >
-        <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-        </View>
-        <TouchableOpacity
-          style={styles.memoDelete}
-          onPress={() => { Alert.alert('Are you sure?'); }}
-        >
-          <Feather name="x" size={16} color="#fff" />
-        </TouchableOpacity>
-      </TouchableOpacity>
-
+      {/* 表示されているところだけを読み込む設定 */}
+      <FlatList
+        data={memos}
+        renderItem={renderItem}
+        // 必ずキーを設定しないといけない
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
+
+MemoList.propTypes = {
+  memos: arrayOf(shape({
+    id: string,
+    bodyText: string,
+    // javascriptから発生したものはinstance
+    updateAt: instanceOf(Date),
+  })).isRequired,
+};
 
 const styles = StyleSheet.create({
   memoListItem: {
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,20 +1,58 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Alert } from 'react-native';
+import firebase from 'firebase';
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  const [memos, setMemos] = useState([]);
   useEffect(() => {
     // ヘッダーに機能を追加
     navigation.setOptions({
       headerRight: () => <LogOutButton />,
     });
   }, []);
+
+  // リストの監視にuseEffectを使う
+  useEffect(() => {
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => { };
+    // ユーザーのログイン情報が消えて実行されなかった時用の処理(if文)
+    if (currentUser) {
+      // ordrerByで順序の並び替え、descは降順のこと、ascは昇順
+      const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
+      // snapshotに自分のメモ情報が入る
+      unsubscribe = ref.onSnapshot((snapshot) => {
+        // 仮の配列
+        const userMemos = [];
+        snapshot.forEach((doc) => {
+          console.log(doc.id, doc.data());
+          // データを加工して格納
+          const data = doc.data();
+          userMemos.push({
+            id: doc.id,
+            bodyText: data.bodyText,
+            updatedAt: data.updatedAt.toDate(),
+          });
+        });
+        // memosにセットする
+        setMemos(userMemos);
+      }, (error) => {
+        // データの読み込みに失敗した時の処理
+        console.log(error);
+        Alert.alert('データの読み込みに失敗しました。');
+      });
+    }
+    // 削除、キャンセルされる時に監視から外れる処理
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MemoList />
+      <MemoList memos={memos} />
       <CircleButton
         name="plus"
         onPress={() => { navigation.navigate('MemoCreate'); }}
